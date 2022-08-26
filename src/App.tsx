268de2751e9a0ea5d05db22825a0b09ac7af64d5
Sebestyen Orsolya <elfromka@gmail.com>
2022-08-26 10:01:39 +0300

    Uploading basic functionality without URL change and URL update on input value of the filter bar on the dataset (which is saved locally in a variable) from the test document

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
-function App() {
-    return <div className="App"></div>;
-}
+import { useState, ChangeEvent } from "react";
+import DATASET from "./data";
+
+const App = () => {
+    const [query, setQuery] = useState<string>("");
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        const { value }: { value: string } = e.target;
+
+        setQuery(value);
+    };
+
+    const dataToDisplay: string[] = DATASET.filter((name) =>
+        name.toLowerCase().includes(query.toLowerCase())
+    );
+
+    return (
+        <main>
+            <section>
+                <h1>Basic search</h1>
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Filter..."
+                    value={query}
+                    onChange={handleChange}
+                />
+                <ul>
+                    {dataToDisplay.length > 0 &&
+                        dataToDisplay
+                            .filter((name) => name)
+                            .map((name) => <li key={name}>{name}</li>)}
+                    {dataToDisplay.length === 0 && <p>No match found</p>}
+                </ul>
+            </section>
+        </main>
+    );
+};
 
 export default App;
